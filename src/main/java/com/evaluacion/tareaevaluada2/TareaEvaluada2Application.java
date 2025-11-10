39b77dd5438b80de59b438bbff32579ae7282b94
Cristian <cristian.jimenez2201@alumnos.ubiobio.cl>
2025-11-10 13:29:32 -0300

    Archivos generados por Spring initializer

---
+++ src/main/java/com/evaluacion/tareaevaluada2/TareaEvaluada2Application.java
@@ -0,0 +1,13 @@
+package com.evaluacion.tareaevaluada2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaEvaluada2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TareaEvaluada2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/evaluacion/tareaevaluada2/TareaEvaluada2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluacion.tareaevaluada2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaEvaluada2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
